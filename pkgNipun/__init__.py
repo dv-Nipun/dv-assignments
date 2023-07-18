29b2b311c3d43e34deaacecaf10a3d4d3426de4d
dv-Nipun <139757778+dv-Nipun@users.noreply.github.com>
2023-07-18 21:50:47 +0530

    Added new feature
    to make password more secure i have added the feature so that the password will include any Name or Places present in CSV file

--- pkgNipun/__init__.py
+++ pkgNipun/__init__.py
@@ -1,25 +1,32 @@
 import random
 import string
 
-def nipuns_password():
+def nipuns_password(exclude_letters):
     length = random.randint(6, 12)
     p1 = []
-    #random digit generated
-    p1.append(random.choice(string.digits)) 
     
-    #here i add random uppercase alphabet
-    p1.append(random.choice(string.ascii_uppercase))  
+    p1.append(random.choice(string.digits))  # random digit generated
+    p1.append(random.choice(string.ascii_uppercase))  # random uppercase alphabet
+    p1.append(random.choice(string.ascii_lowercase))  # random lowercase alphabet
     
-    #here i add random lowercase alphabet
-    p1.append(random.choice(string.ascii_lowercase))
-    
-
     for i in range(length - 3):
-        p1.append(random.choice(string.ascii_letters + string.digits))
-        #now i joined whole password in single string
-
+        char = random.choice(string.ascii_letters + string.digits)
+        while char in exclude_letters:
+            char = random.choice(string.ascii_letters + string.digits)
+        p1.append(char)
+    
     return ''.join(p1)
 
-p1 = nipuns_password()
-print(p1)
+# Read excluded letters from "place.csv" and "name.csv"
+with open('Place.csv', 'r') as file:
+    excluded_place = file.read().strip()
 
+with open('Name.csv', 'r') as file:
+    excluded_name = file.read().strip()
+
+# Combine excluded letters from both files
+excluded_letters = excluded_place + excluded_name
+
+# Generate password excluding the letters from the files
+p1 = nipuns_password(excluded_letters)
+print(p1)
