aecbdffc457ce41b8f0ba0069875764ccd96c8c7
dv-Nipun <139757778+dv-Nipun@users.noreply.github.com>
2023-07-20 22:11:23 +0530

    Update testing.py

--- pkgNipun/testing.py
+++ pkgNipun/testing.py
@@ -1,25 +1,21 @@
 import unittest
-from Password import generate_password , get_excluded_letters
 
-class TestPasswordGenerator(unittest.TestCase):
+from Password import generate_password
 
-    def setUp(self):
-        self.place_file_path = 'Place.csv'  # Replace with the path to your Place.csv file
-        self.name_file_path = 'Name.csv'    # Replace with the path to your Name.csv file
 
-    def test_generate_password_length(self):
-        # Test if the generated password has the correct length
-        excluded_letters = get_excluded_letters(self.place_file_path) + get_excluded_letters(self.name_file_path)
-        password = generate_password(excluded_letters)
-        self.assertGreaterEqual(len(password), 6)
-        self.assertLessEqual(len(password), 12)
+class GeneratePasswordTest(unittest.TestCase):
 
-    def test_generate_password_excluded_letters(self):
-        # Test if the generated password does not contain any excluded letters
-        excluded_letters = get_excluded_letters(self.place_file_path) + get_excluded_letters(self.name_file_path)
-        password = generate_password(excluded_letters)
-        for char in password:
-            self.assertNotIn(char, excluded_letters)
+    def test_generate_password_with_no_excluded_strings(self):
+        excluded_strings = set()
+        generated_password = generate_password(excluded_strings)
+        self.assertRegex(generated_password, r'[a-zA-Z0-9]{6,12}')
+
+    def test_generate_password_with_excluded_strings(self):
+        excluded_strings = {'password', '123456', 'helloworld'}
+        generated_password = generate_password(excluded_strings)
+        self.assertNotIn(generated_password, excluded_strings)
+        self.assertRegex(generated_password, r'[a-zA-Z0-9]{6,12}')
+
 
 if __name__ == '__main__':
     unittest.main()
